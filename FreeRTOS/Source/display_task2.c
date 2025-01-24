bf9450738ad27aa9e53194fbfe2dc6091cb07a2d
Franco Viotti <fviotti@unc.edu.ar>
2025-01-24 16:37:00 -0300

    display shows bit associated with temperature and time has only 2 digits

--- FreeRTOS/Source/display_task2.c
+++ FreeRTOS/Source/display_task2.c
@@ -1,5 +1,6 @@
 #include "display_task2.h"
 #include "filter_task.h"
+#include "utils.h"
 
 #define DISPLAY_WIDTH 96
 #define DISPLAY_HEIGHT 16
@@ -7,6 +8,19 @@
 
 extern QueueHandle_t xFilteredTempQueue;
 extern int currentN;
+
+static void getBitForTemperature(int temp, unsigned char *point) {
+  point[0] = 0x00;  // byte superior
+  point[1] = 0x80;  // byte inferior con el bit del eje X
+  
+  int bit_position = 7 - ((temp - TEMP_MIN) * 8) / (TEMP_MAX - TEMP_MIN);
+  
+  if (bit_position < 8) {
+    point[0] |= (1 << bit_position);
+  } else {
+    point[1] |= (1 << (bit_position - 8));
+  }
+}
 
 static void vDisplayTask(void *pvParameters) {
   char str[10];
@@ -25,7 +39,7 @@ static void vDisplayTask(void *pvParameters) {
       OSRAMStringDraw("t:", 0, 1);
       int_to_string(newData.temperature, str);
       OSRAMStringDraw(str, 10, 0);
-      long_to_string(newData.time_ms/1000, str);
+      long_to_string(newData.time_ms/1000 % 100, str);
       OSRAMStringDraw(str, 10, 1);
       
       // Dibujar eje Y
@@ -34,9 +48,13 @@ static void vDisplayTask(void *pvParameters) {
       
       // Dibujar eje X
       unsigned char x_axis[] = {0x80}; // Un solo byte
-      for (int i = GRAPH_START_X + 5; i < DISPLAY_WIDTH; i++) {
+      for (int i = x_start; i < DISPLAY_WIDTH; i++) {
         OSRAMImageDraw(x_axis, i, 1, 1, 1);
       }
+
+      unsigned char temp_point[2];
+      getBitForTemperature(newData.temperature, temp_point);
+      OSRAMImageDraw(temp_point, x_start, 0, 1, 2);
     }
     vTaskDelay(pdMS_TO_TICKS(500));
   }
