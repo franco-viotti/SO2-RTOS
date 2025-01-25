54acf3dbbba31cb2363d0a9eca12951ab9444c26
Franco Viotti <fviotti@unc.edu.ar>
2025-01-25 02:42:13 -0300

    file reorganization

--- FreeRTOS/Source/include/display_task.h
+++ FreeRTOS/Source/include/display_task.h
@@ -3,17 +3,8 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "utils.h"
+#include "queue.h"
 
-// Definiciones para el display
-#define DISPLAY_WIDTH    96  // Ancho del OLED
-#define DISPLAY_HEIGHT   16  // Alto del OLED
-// #define MAX_POINTS 96  // Ancho del display
-
-// Declaración externa de la cola que usaremos
-extern QueueHandle_t xFilteredTempQueue;
-
-// Función para crear la tarea
 void vStartDisplayTask(void);
 
-#endif
+#endif // DISPLAY_TASK_H
